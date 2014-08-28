db4378dfd78d2b99026710fd371f23c812e46892
Zongheng Yang <zongheng.y@gmail.com>
2014-08-27 23:59:06 -0700

    Typo.

--- stream-ec2-bench/run-stream.sh
+++ stream-ec2-bench/run-stream.sh
@@ -7,4 +7,4 @@ summary_file=stream-res-$instance_type-$num_threads-summary.txt
 
 gcc -O -DSTREAM_ARRAY_SIZE=$arr_size -fopenmp -D_OPENMP stream.c -o stream
 OMP_NUM_THREADS=$num_threads ./stream >$log_file
-tail -8 log_file | head -5 >$summary_file
+tail -8 $log_file | head -5 >$summary_file
