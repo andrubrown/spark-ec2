6bb80c7c49c8c7bceee04d024e0c82517bc724c6
Zongheng Yang <zongheng.y@gmail.com>
2014-08-27 23:43:25 -0700

    Use file vars.

--- stream-ec2-bench/run-stream.sh
+++ stream-ec2-bench/run-stream.sh
@@ -6,5 +6,5 @@ log_file=stream-res-$instance_type-$num_threads-log.txt
 summary_file=stream-res-$instance_type-$num_threads-summary.txt
 
 gcc -O -DSTREAM_ARRAY_SIZE=$arr_size -fopenmp -D_OPENMP stream.c -o stream
-OMP_NUM_THREADS=$num_threads ./stream >log_file
-tail -8 log_file | head -5 >summary_file
+OMP_NUM_THREADS=$num_threads ./stream >$log_file
+tail -8 log_file | head -5 >$summary_file
